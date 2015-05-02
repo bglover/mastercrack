62086e809f25afe67b8ae3d7c1fcf898d83e5cea
Brian Glover <brian@access9.net>
2015-05-02 15:33:50 -0500

    API Changes and New Binary.
    - Add ascii art
    - Deprecated setting properties directly
    - Added binary to composer install.
    - Using CLImate for cli processing.

--- README.md
+++ README.md
@@ -1,37 +1,120 @@
 # MasterCrack
+<pre>
+             _,,....,,_
+          ,:`,11fLG1i:;;`.
+        ,,101,:ti:;i::f0i;:          A PHP class to calculate the
+       ,,0t::         :,C01i         combination for a masterlock.
+      :;0t:             :f01;
+     iLGL;               t0Li,       Uses a technique by <a href=http://null-byte.wonderhowto.com/how-to/crack-any-master-combination-lock-8-tries-less-using-calculator-0161629/">Samy Kamkar</a>
+     tL0L1               1L01;
+     1L0Li               iL8f;
+     tL0L;               iL0L;
+     tL0L;               iL8Li
+     1L01t.:..,::,i1::;i,:,8Li
+     iL,:1:;1tt:;;;,CCf1iiiiLi
+    .,it1t;,,.,if0fi:,;;iii11:.
+   ,:t,,,,t00tG;G;G;G1GGtii;1fti
+  :f;,,10f0LGGGGGfGGGGC0LG0t;i1t1
+ 1f;.;GGiG0L0GGGLiGGG0000fG0Gti1t1
+:Ltit80C0:ftt0CCCGGG000C1G8880ti1f,
+tL1f088800LCLLGLCCCG8GC880808G0t1f1
+LtttG888888 ~~~~~~~~~~~ 888880Gt111
+:ttC008:i00 MASTERCRACK 0tLC0GCCii:
+.::LGC0:fGG ~~~~~~~~~~~ GG:1GLGL;:.
+ ,:i8LCGGGCCCGGGGGGGGG0GGGGGG18i::
+  ,:f0:GGLfCGGGGGGGGGGGt1GGG,0L::
+   ,:f8;GCfLfGGGGG0GG0i;;LGt81;:
+     ::GCG;GGGGGGfLG0000GG0C;i
+       ,;f8f0t0G0:010i0t8fii
+         '.ii1LG888GLii1:'
+</pre>
 
-A PHP class to calculate the combination for a masterlock using a technique
-discovered by [Samy Kamkar](http://null-byte.wonderhowto.com/how-to/crack-any-master-combination-lock-8-tries-less-using-calculator-0161629/)
+
+## Installation
+
+### Prerequisites
+
+MasterCrack requires PHP 5.4 or higher.
 
 
-# Installation
-```sh
-composer require access9\mastercrack
+### Composer (directly)
+
+The easiest way to install this tool is via composer
+
+``` shell
+composer require access9/mastercrack
 ```
 
 
-# Usage Example
+### Composer (via your composer.json)
 
-```php
-#!env php
-<?php
-require_once 'vendor/autoload.php';
+Add the following to your composer.json
 
-$m = new Access9\MasterCrack();
-
-echo 'Lock Position First: ';
-$m->lp1 = trim(fgets(STDIN));
-
-echo 'Lock Position Second: ';
-$m->lp2 = trim(fgets(STDIN));
-
-echo 'Resistance: ';
-$m->resist = trim(fgets(STDIN));
-
-// Calculate the combination.
-$m->calc();
-
-echo PHP_EOL, 'first: ',  $m->getFirst(), PHP_EOL;
-echo 'second: ', implode(', ', $m->getSecond()), PHP_EOL;
-echo 'third: ',  implode(', ', $m->getThird()), PHP_EOL;
+``` json
+"access9/mastercrack": "*"
 ```
+
+#### When either of the two composer installation methods completes, you can run mastercrack with:
+
+``` shell
+vendor/bin/mastercrack
+```
+
+
+### Cloning
+
+Of course you can always clone the repository:
+
+``` shell
+git clone https://github.com/bglover/mastercrack.git
+```
+
+Make sure you get the required vendor dependencies using composer:
+
+``` shell
+composer update -o --prefer-dist
+```
+
+
+# Usage
+
+```shell
+vendor/bin/mastercrack
+
+1. Find the "First Locked Position"
+  1. Set the dial to 0.
+  2. Apply full pressure upward on the shackle as if trying to open it.
+  3. Rotate dial to the left (towards 10) hard until the dial gets locked.
+  4. Notice how the dial is locked into a small groove.
+     a. If you're directly between two digits such as 3 and 4,
+        release the shackle and turn the dial left further until
+        you're into the next locked groove.
+     b. However, if the dial is between two half digits (e.g., 2.5 and 3.5),
+        then enter the digit in-between (e.g., 3) into First Locked Position below.
+
+First Locked Position: 4
+
+2. Find the "Second Locked Position"
+  Do all of the above again until you find the second digit below 11 that is
+  between two half digits (e.g., 5.5 and 6.5), and enter the whole number (e.g., 7)
+  into Second Locked Position below.
+
+Second Locked Position: 7
+
+3. Find the "Resistant Location"
+  1. Apply half as much pressure to the shackle so that you can turn the dial.
+  2. Rotate dial to the right until you feel resistance.
+     - Rotate the dial to the right several more times to ensure you're feeling
+       resistance at the same exact location.
+  3. Enter this number into Resistant Location below.
+     - If the resistance begins at a half number, such as 14.5, enter 14.5.
+
+Resistant Location: 27
+
+First:  32
+Second: 2, 6, 10, 14, 18, 22, 26, 30, 34, 38
+Third:  4, 24
+```
+
+# Credits
+Only the PHP code was written by me. The forumla and help text were taken from the website [How to crack any master combination lock in 8 tries or less](http://null-byte.wonderhowto.com/how-to/crack-any-master-combination-lock-8-tries-less-using-calculator-0161629)
--- src/MasterCrack.php
+++ src/MasterCrack.php
@@ -11,17 +11,17 @@ class MasterCrack
     /**
      * @var float
      */
-    private $lp1;
+    private $firstLockPosition;
 
     /**
      * @var float
      */
-    private $lp2;
+    private $secondLockPosition;
 
     /**
-     * @var int|float
+     * @var float
      */
-    private $resist;
+    private $resistance;
 
     /**
      * @var int
@@ -48,14 +48,137 @@ class MasterCrack
     /**
      * Public setter
      *
+     * @deprecated
      * @param string $key
      * @param mixed  $val
      */
     public function __set($key, $val)
     {
-        if (!in_array($key, ['first', 'second', 'third'])) {
-            $this->{$key} = $val;
+        switch ($key) {
+            case 'lp1':
+                $this->setFirstLockPosition($val);
+                break;
+            case 'lp2':
+                $this->setSecondLockPosition($val);
+                break;
+            case 'resist':
+                $this->setResistance($val);
+                break;
+            default:
+                break;
         }
+
+        trigger_error(
+            'Direct access to class properties will be removed in a future release. '
+            . 'Use the appropriate set method instead.',
+            E_USER_DEPRECATED
+        );
+    }
+
+    /**
+     * Set the value of the First Lock Position.
+     *
+     * @param float $val
+     * @return MasterCrack
+     */
+    public function setFirstLockPosition($val)
+    {
+        $this->validatePosition($val);
+        $this->firstLockPosition = $val;
+        return $this;
+    }
+
+    /**
+     * Set the value of the Second Lock Position.
+     *
+     * @param float $val
+     * @return MasterCrack
+     */
+    public function setSecondLockPosition($val)
+    {
+        $this->validatePosition($val);
+        $this->secondLockPosition = $val;
+        return $this;
+    }
+
+    /**
+     * @param float|int $resistance
+     * @return MasterCrack
+     */
+    public function setResistance($resistance)
+    {
+        $this->resistance = $resistance;
+
+        return $this;
+    }
+
+    /**
+     * Validate that the given input is between 1 and 10.
+     *
+     * @throws \InvalidArgumentException
+     * @param int|float $val
+     */
+    private function validatePosition($val)
+    {
+        if ($val < 1 || $val > 10) {
+            throw new \InvalidArgumentException(
+                'Position must be a number between 1 and 10 (inclusive)'
+            );
+        }
+    }
+
+    /**
+     * Perform the calculation.
+     *
+     * @throws UnexpectedValueException
+     * @return MasterCrack
+     */
+    public function calc()
+    {
+        foreach (['firstLockPosition', 'secondLockPosition', 'resistance'] as $var) {
+            if (!isset($this->{$var})) {
+                throw new UnexpectedValueException(
+                    'Can not continue: ' . $var . ' value not set.'
+                );
+            }
+        }
+
+        $this->setFirst();
+        $this->setMod();
+        $this->setThird();
+        $this->setSecond();
+
+        return $this;
+    }
+
+    /**
+     * Get the first combination value.
+     *
+     * @return int
+     */
+    public function getFirst()
+    {
+        return $this->first;
+    }
+
+    /**
+     * Get the second combination value(s).
+     *
+     * @return array
+     */
+    public function getSecond()
+    {
+        return $this->second;
+    }
+
+    /**
+     * Get the third combination value(s).
+     *
+     * @return array
+     */
+    public function getThird()
+    {
+        return $this->third;
     }
 
     /**
@@ -65,13 +188,13 @@ class MasterCrack
      */
     private function setFirst()
     {
-        if (!isset($this->resist)) {
+        if (!isset($this->resistance)) {
             throw new UnexpectedValueException(
                 'The resistance must be set before the first combination can be calculated.'
             );
         }
 
-        $this->first = (ceil($this->resist) + 5) % 40;
+        $this->first = (ceil($this->resistance) + 5) % 40;
 
         return $this;
     }
@@ -119,8 +242,8 @@ class MasterCrack
 
             if ($checkDigit) {
                 $key        = $checkDigit - 1;
-                $testFirst  = $tmp != (($this->third[$key] + 2) % 40);
-                $testSecond = $tmp != (($this->third[$key] - 2) % 40);
+                $testFirst  = $tmp !== (($this->third[$key] + 2) % 40);
+                $testSecond = $tmp !== (($this->third[$key] - 2) % 40);
             }
 
             if (!$checkDigit || ($testFirst && $testSecond)) {
@@ -139,72 +262,17 @@ class MasterCrack
     private function setThird()
     {
         for ($i = 0; $i < 4; $i++) {
-            $lp1Test = ((10 * $i) + $this->lp1) % 4;
-            if ($lp1Test == $this->mod) {
-                $this->third[] = (10 * $i) + $this->lp1;
+            $lockPositionOneTest = ((10 * $i) + $this->firstLockPosition) % 4;
+            if ($lockPositionOneTest === $this->mod) {
+                $this->third[] = (10 * $i) + $this->firstLockPosition;
             }
 
-            $lp2Test = ((10 * $i) + $this->lp2) % 4;
-            if ($lp2Test == $this->mod) {
-                $this->third[] = (10 * $i) + $this->lp2;
+            $lockPositionTwoTest = ((10 * $i) + $this->secondLockPosition) % 4;
+            if ($lockPositionTwoTest === $this->mod) {
+                $this->third[] = (10 * $i) + $this->secondLockPosition;
             }
         }
 
         return $this;
-    }
-
-    /**
-     * Perform the calculation.
-     *
-     * @return $this
-     * @throws UnexpectedValueException
-     * @return MasterCrack
-     */
-    public function calc()
-    {
-        foreach (['lp1', 'lp2', 'resist'] as $var) {
-            if (!isset($this->{$var})) {
-                throw new UnexpectedValueException(
-                    'Can not continue: ' . $var . ' value not set.'
-                );
-            }
-        }
-
-        $this->setFirst();
-        $this->setMod();
-        $this->setThird();
-        $this->setSecond();
-
-        return $this;
-    }
-
-    /**
-     * Get the first combination value.
-     *
-     * @return int
-     */
-    public function getFirst()
-    {
-        return $this->first;
-    }
-
-    /**
-     * Get the second combination value(s).
-     *
-     * @return array
-     */
-    public function getSecond()
-    {
-        return $this->second;
-    }
-
-    /**
-     * Get the third combination value(s).
-     *
-     * @return array
-     */
-    public function getThird()
-    {
-        return $this->third;
     }
 }
